5693f2c16d781c7a240ae888a4f75f47bbab625b
“[Martialeagle <k.l.ashwanthram@gmail.com>
2022-10-19 23:17:15 +0530

    constructor and prototype

--- main.js
+++ main.js
@@ -6,3 +6,28 @@ const printNums = (a = 421, b=73) => {
 
 
 printNums(1);
+
+function person(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+
+person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+
+const person1 = new person('alice', 'smith', '12-19-2002');
+const person2 = new person('John', 'Jacobs', '1-12-2012');
+
+
+console.log(person1);
+console.log(person2.getFullName());
