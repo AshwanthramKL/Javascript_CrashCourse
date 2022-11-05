6e078b347663d128f4479701f9baadbcaa73b53e
“[Martialeagle <k.l.ashwanthram@gmail.com>
2022-11-05 12:32:25 +0530

    completed form application

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-// Event Listener
+const form = document.querySelector('#my-form');
+const message = document.querySelector('.msg');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const userList = document.querySelector('#users');
 
-button = document.querySelector('.btn');
+form.addEventListener('submit', onSubmit);
 
-button.addEventListener('mouseout', (e) =>{
-    e.preventDefault();
-    document.querySelector('.btn').style.background = '#cc12fc';
-    document.querySelector('.container').classList.add('bg-dark');
-})
+function onSubmit(e){
+    e.preventDefault()
+    if(nameInput.value === '' || emailInput.value === '')
+    {
+        message.classList.add('error')
+        message.innerHTML = '<p>Please enter all fields</p>'
+        
+        setTimeout(() => message.remove(), 1000);
+    }
+
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
