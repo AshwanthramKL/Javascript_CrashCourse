be17ef987acd1b0ad83d454a97d54dec8ddef0a6
“[Martialeagle <k.l.ashwanthram@gmail.com>
2022-11-05 12:03:48 +0530

    event listener

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-// DOM manipulation
-
-const ul = document.querySelector('.items');
-
-console.log(ul)
-// ul.lastElementChild.remove();
-// ul.lastElementChild.remove();
-// ul.lastElementChild.remove();
-
-ul.firstElementChild.textContent = 'Hello world';
-
-ul.children[1].innerText = 'Mark';
-ul.children[1].style.background = 'black';
-
-ul.children[1].style.color = 'orange';
-
-
-
-ul.lastElementChild.innerHTML = '<h1>Dummy text</h1><p> aklsdjfklhjweiohfio iasdOHJFIOAWEHIOFAS DLIFJIASD FJOIj sidjfai k</p>'
+// Event Listener
 
 button = document.querySelector('.btn');
 
-button.style.background = 'green'
+button.addEventListener('mouseout', (e) =>{
+    e.preventDefault();
+    document.querySelector('.btn').style.background = '#cc12fc';
+    document.querySelector('.container').classList.add('bg-dark');
+})
