81446c9de9f9c38559591fef9f50787c55b13742
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-01-08 15:21:49 +0530

    querySelector

--- DOM.js
+++ DOM.js
@@ -61,5 +61,22 @@
 
 // QuerySelector - Works pretty much like jquery
 
+var header = document.querySelector('#main-header'); // can pass any css query as parameter
 
+header.style.borderBottom = 'solid 4px #ccc';
 
+var input = document.querySelector('input'); // grabs only the first one
+
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'SEND';
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'brown';
